f3c0c20cdddac7c6db52f077e3c621d7cbfcece7
Sergey Kuznetsov <kuzzz99@gmail.com>
2019-09-18 23:07:39 +0300

    Fix errors. Remove TypeOf parameters. Fix const char* deducing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 template <std::size_t N> class StaticString {
 public:
@@ -17,79 +16,69 @@ constexpr StaticString<N> CreateStaticString(const char (&s)[N]) {
 }
 
 template <std::size_t N1, std::size_t N2>
-constexpr auto Concat(const StaticString<N1> &s1, const StaticString<N2> &s2) {
+constexpr auto operator+(StaticString<N1> lhs, StaticString<N2> rhs) {
   char result[N1 + N2 - 1] = {0};
   for (std::size_t i = 0; i < N1 - 1; ++i)
-    result[i] = s1.str[i];
+    result[i] = lhs.str[i];
   std::size_t j = 0;
   for (std::size_t i = N1 - 1; i < N1 + N2 - 2; ++i, ++j)
-    result[i] = s2.str[j];
+    result[i] = rhs.str[j];
   return CreateStaticString(result);
 }
 
-template <class T> constexpr auto TypeOf(T) {
-  return CreateStaticString("unknown");
-}
+template <class T> struct RemoveConstFromPointer { using type = T; };
+template <class T> struct RemoveConstFromPointer<const T *> {
+  using type = T *;
+};
 
-//template <> constexpr auto TypeOf<wint_t>(wint_t) {
+template <class T> constexpr auto TypeOf() { CreateStaticString("unknown"); }
+
+// template <> constexpr auto TypeOf<wint_t>(wint_t) {
 //  return CreateStaticString("%lc");
 //}
-template <> constexpr auto TypeOf<char *>(char *) {
-  return CreateStaticString("%s");
-}
+template <> constexpr auto TypeOf<char *>() { return CreateStaticString("%s"); }
 // template <> constexpr auto TypeOf<wchar_t*>(wchar_t*) { return "%ls"); }
-template <> constexpr auto TypeOf<char>(char) {
-  return CreateStaticString("%c");
-}
-template <> constexpr auto TypeOf<short>(short) {
-  return CreateStaticString("%hd");
-}
-template <> constexpr auto TypeOf<int>(int) { return CreateStaticString("%d"); }
-template <> constexpr auto TypeOf<long>(long) {
-  return CreateStaticString("%ld");
-}
-template <> constexpr auto TypeOf<long long>(long long) {
+template <> constexpr auto TypeOf<char>() { return CreateStaticString("%c"); }
+template <> constexpr auto TypeOf<short>() { return CreateStaticString("%hd"); }
+template <> constexpr auto TypeOf<int>() { return CreateStaticString("%d"); }
+template <> constexpr auto TypeOf<long>() { return CreateStaticString("%ld"); }
+template <> constexpr auto TypeOf<long long>() {
   return CreateStaticString("%lld");
 }
-//template <> constexpr auto TypeOf<intmax_t>(intmax_t) {
+// template <> constexpr auto TypeOf<intmax_t>(intmax_t) {
 //  return CreateStaticString("%jd");
 //}
-template <> constexpr auto TypeOf<unsigned short>(unsigned short) {
+template <> constexpr auto TypeOf<unsigned short>() {
   return CreateStaticString("%hu");
 }
-template <> constexpr auto TypeOf<unsigned int>(unsigned int) {
+template <> constexpr auto TypeOf<unsigned int>() {
   return CreateStaticString("%u");
 }
-template <> constexpr auto TypeOf<unsigned long>(unsigned long) {
+template <> constexpr auto TypeOf<unsigned long>() {
   return CreateStaticString("%lu");
 }
-template <> constexpr auto TypeOf<unsigned long long>(unsigned long long) {
+template <> constexpr auto TypeOf<unsigned long long>() {
   return CreateStaticString("%llu");
 }
-template <> constexpr auto TypeOf<float>(float) {
-  return CreateStaticString("%f");
-}
-template <> constexpr auto TypeOf<double>(double) {
-  return CreateStaticString("%f");
-}
-template <> constexpr auto TypeOf<long double>(long double) {
+template <> constexpr auto TypeOf<float>() { return CreateStaticString("%f"); }
+template <> constexpr auto TypeOf<double>() { return CreateStaticString("%f"); }
+template <> constexpr auto TypeOf<long double>() {
   return CreateStaticString("%Lf");
 }
 
-template <class T, class... Args>
-constexpr auto TypeOf(const T &t, const Args &... args) {
-  const auto s1 = TypeOf(t);
-  const auto s2 = TypeOf(args...);
-  const auto r = Concat(s1, s2);
-  return r;
+template <class T> constexpr auto TypeOfCool() {
+  if constexpr (std::is_pointer<T>::value)
+    return TypeOf<typename RemoveConstFromPointer<T>::type>();
+  else
+    return TypeOf<T>();
 }
 
-template <class... Args> constexpr auto PrintfFormating(Args... args) {
-  return TypeOf(args...);
+template <class... Args> constexpr auto PrintfFormatting(Args... args) {
+  return (... + TypeOfCool<Args>());
 }
 
 int main() {
-  constexpr auto s = PrintfFormating("abc", 1, 2.f, 3., 2l, 3u);
+  constexpr auto s = PrintfFormatting("abc", 1, 2.f, 3., 2l, 3u);
   std::cout << s.str << std::endl;
   return 0;
 }
